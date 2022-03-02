2fb51340379b4fef80a7de1553cae764797f1d3d
Tygoog24 <97486494+Tygoog24@users.noreply.github.com>
2022-03-01 21:18:15 -0500

    first commit

---
+++ index.js
@@ -0,0 +1,34 @@
+//one way we could do it
+var character="1234567890abcdefghijklmnopqrstuvxyz!#$%&'()*+,-./:;<=>?@[]^_`{|}~"
+var passwordlength="128"
+
+//create a formula to generate random password
+for (var i = 0; i <passwordlength; i++ ){
+    var randomnumber = Math.floor(Math.random()) * character.length);
+    password
+}
+------------//ignore the above its one way we could do it
+
+// Create variables for passwords
+//seperate the lower case from uppercase
+let lowercase="abcdefghijklmnopqrstuvxyz"
+//seperat the numbers 
+let numbers="1234567890"
+//seperate the symbols
+let symbols="!#$%&'()*+,-./:;<=>?@[]^_`{|}~"
+let uppercase="ABCDEFGHIJKLMNOPQRSTUV"
+
+
+//create function to copy text
+function copytext {
+//function will go here
+}
+
+//create function to generate password
+function generatepassword{
+//function will go here
+
+//create an alert so they know they copied text
+alert("password copied")
+}
+
