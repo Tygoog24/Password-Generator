20b4b72bf270bc1acadbf8fa38586cf7a4a262cd
Tygoog24 <97486494+Tygoog24@users.noreply.github.com>
2022-03-02 19:57:46 -0500

    Update index.js

--- index.js
+++ index.js
@@ -7,17 +7,24 @@
 //when they click generate password they are supposed to get a combination of letters uppercase/lowercase, symbols and numbers
 //when they press generate password an assortment of symbol, letters, and numbers will appear and they copy the information from the page
 
-const includeNumbersElement = document.getElementById('includeNumbers')
-const includeSymbolsElement = document.getElementById('includeSymbols')
-const includeUppercaseElement = document.getElementById('includeUppercase')
-const includeLowercaseElement = document.getElementById('includeLowercase')
-const form = document.getElementById('passwordGeneratorForm')
-const characterAmountNumber = document.getElementById('slider')
+const includeNumbersElement = document.getElementById('includeNumbers');
+const includeSymbolsElement = document.getElementById('includeSymbols');
+const includeUppercaseElement = document.getElementById('includeUppercase');
+const includeLowercaseElement = document.getElementById('includeLowercase');
+const form = document.getElementById('passwordGeneratorForm');
+const characterAmountNumber = document.getElementById('slider');
+const submit = document.getElementById('submit');
 
+let symbolstr = '!@#$%^&*()-';
+let uppercasestr = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+let lowercasestr = 'abcdefghijklmnopqrstuvwxyz';
+let numbersstr = '1234567890';
 
-
-
-
+submit.addEventListener ('click', ()=>  {
+    if(uppercasestr.checked && lowercasestr.checked && numbersstr.checked && symbolstr.checked) {
+        let random = Math.floor(Math.random() * 10);
+    }    
+})
 
 
 
