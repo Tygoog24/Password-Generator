fbe0d1b1251feecef6544b6b2731dee5e040b60c
Tygoog24 <97486494+Tygoog24@users.noreply.github.com>
2022-03-02 19:22:16 -0500

    Update index.js

--- index.js
+++ index.js
@@ -7,5 +7,17 @@
 //when they click generate password they are supposed to get a combination of letters uppercase/lowercase, symbols and numbers
 //when they press generate password an assortment of symbol, letters, and numbers will appear and they copy the information from the page
 
+const includeNumbersElement = document.getElementById('includeNumbers')
+const includeSymbolsElement = document.getElementById('includeSymbols')
+const includeUppercaseElement = document.getElementById('includeUppercase')
+const includeLowercaseElement = document.getElementById('includeLowercase')
+const form = document.getElementById('passwordGeneratorForm')
+const characterAmountNumber = document.getElementById('slider')
+
+
+
+
+
+
 
 
