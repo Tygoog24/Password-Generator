d2012a94e36870a708cf3c5af1ae147706ff52c4
Tygoog24 <97486494+Tygoog24@users.noreply.github.com>
2022-03-03 21:58:23 -0500

    readded finshed pseudocode
    Added the logic for our pseudocode

--- index.js
+++ index.js
@@ -1,3 +1,12 @@
+// when a person selects one criteria for uppercase they are supposed to get an assortment of letters that are capitalized
+// when a person selects one criteria for lowercase they get an assortment of letters that are lowercased
+// when a person selects one criteria for symbols they get an assortment of symbols
+// when a person selects one criteria for numbers they get an assortment of numbers
+//when they move the range bar they can determine the amount characters for their password
+//when they click generate password we have to first check that one criteria has been selected
+//when they click generate password they are supposed to get a combination of letters uppercase/lowercase, symbols and numbers
+//when they press generate password an assortment of symbol, letters, and numbers will appear and they copy the information from the page
+
 const includeNumbers = document.getElementById('includeNumbers');
 const includeSymbols = document.getElementById('includeSymbols');
 const includeUppercase = document.getElementById('includeUppercase');
