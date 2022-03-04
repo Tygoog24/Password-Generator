696925816383fb01070ec9fd5b99c0a57e739628
Tygoog24 <97486494+Tygoog24@users.noreply.github.com>
2022-03-03 20:37:45 -0500

    created function

--- index.js
+++ index.js
@@ -6,7 +6,7 @@
 //when they click generate password we have to first check that one criteria has been selected
 //when they click generate password they are supposed to get a combination of letters uppercase/lowercase, symbols and numbers
 //when they press generate password an assortment of symbol, letters, and numbers will appear and they copy the information from the page
-
+/*
 const includeNumbersElement = document.getElementById('includeNumbers');
 const includeSymbolsElement = document.getElementById('includeSymbols');
 const includeUppercaseElement = document.getElementById('includeUppercase');
@@ -15,16 +15,23 @@ const form = document.getElementById('passwordGeneratorForm');
 const characterAmountNumber = document.getElementById('slider');
 const submit = document.getElementById('submit');
 
-let symbolstr = '!@#$%^&*()-';
-let uppercasestr = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
-let lowercasestr = 'abcdefghijklmnopqrstuvwxyz';
-let numbersstr = '1234567890';
-
-submit.addEventListener ('click', ()=>  {
-    if(uppercasestr.checked && lowercasestr.checked && numbersstr.checked && symbolstr.checked) {
-        let random = Math.floor(Math.random() * 10);
-    }    
-})
+let symbol = '!@#$%^&*()-';
+let uppercase = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+let lowercase = 'abcdefghijklmnopqrstuvwxyz';
+let numbers= '1234567890';
 
 
+let includeUppercase = document.getElementById('includeUppercase');
+console.log (includeUppercase);
+*/
 
+function generatepassword () {
+    //lets the computer know all the variables we want pulled
+    let allcharacters= "ABCDEFGHIJKLMNOPQRSTUVWXYZ!@#$%^&*()-123456789abcdefghijklmnopqrstuvwxyz" ;
+    //we are trying to slice characters and get a string of values that are equal to the password length
+   let random = Math.floor(Math.random() * 20);
+   let newchar = characters.slice(random, 20);
+   console.log (newchar);
+}
+
+generatepassword();
