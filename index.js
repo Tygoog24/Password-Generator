594872fe5168a61a4195af085adf27b6393d9f88
Tygoog24 <97486494+Tygoog24@users.noreply.github.com>
2022-03-03 21:56:09 -0500

    fact check console log for if else statements
    We went through to see if our console was printing information

--- index.js
+++ index.js
@@ -1,12 +1,3 @@
-// when a person selects one criteria for uppercase they are supposed to get an assortment of letters that are capitalized
-// when a person selects one criteria for lowercase they get an assortment of letters that are lowercased
-// when a person selects one criteria for symbols they get an assortment of symbols
-// when a person selects one criteria for numbers they get an assortment of numbers
-//when they move the range bar they can determine the amount characters for their password
-//when they click generate password we have to first check that one criteria has been selected
-//when they click generate password they are supposed to get a combination of letters uppercase/lowercase, symbols and numbers
-//when they press generate password an assortment of symbol, letters, and numbers will appear and they copy the information from the page
-
 const includeNumbers = document.getElementById('includeNumbers');
 const includeSymbols = document.getElementById('includeSymbols');
 const includeUppercase = document.getElementById('includeUppercase');
@@ -28,7 +19,14 @@ function generatepassword () {
     let allcharacters= "ABCDEFGHIJKLMNOPQRSTUVWXYZ!@#$%^&*()-123456789abcdefghijklmnopqrstuvwxyz" ;
     //we are trying to slice characters and get a string of values that are equal to the password length
    let random = Math.floor(Math.random() * 20);
+   let newchar = allcharacters.slice(random, 20);
+   console.log (newchar);
    console.log (allcharacters);
+   console.log (uppercase);
+   console.log (uppercase + lowercase);
+   console.log (numbers);
+   console.log (symbol);
+   
 
    if(includeUppercase.checked && includeLowercase.checked && includeNumbers.checked && includeSymbols.checked) {
     let random = Math.floor(Math.random() * 20);
@@ -36,6 +34,7 @@ function generatepassword () {
     let allcharacters= "ABCDEFGHIJKLMNOPQRSTUVWXYZ!@#$%^&*()-123456789abcdefghijklmnopqrstuvwxyz" ;
     // pulling the inner text of the h3 and changing it into the new sliced string
     document.getElementById('showpassword').innerText = allcharacters.slice(random, random + +passwordLength.value);
+    console.log(allcharacters.slice(random, random + +passwordLength.value));
 }
     else if(includeUppercase.checked && includeLowercase.checked) {
         let random = Math.floor(Math.random() * 20);
@@ -72,10 +71,7 @@ function generatepassword () {
         let allcharacters = lowercase + numbers;
         document.getElementById('showpassword').innerText = allcharacters.slice(random, random + +passwordLength.value);
     }
-
-
 }
-
 
 
 generatepassword();
