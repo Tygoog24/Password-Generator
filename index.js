d673ac3c406431ff6be7522a69c3f4a8ee461e89
Tygoog24 <97486494+Tygoog24@users.noreply.github.com>
2022-03-05 16:52:39 -0500

    first commit

--- index.js
+++ index.js
@@ -6,13 +6,9 @@
 //when they click generate password we have to first check that one criteria has been selected
 //when they click generate password they are supposed to get a combination of letters uppercase/lowercase, symbols and numbers
 //when they press generate password an assortment of symbol, letters, and numbers will appear and they copy the information from the page
-<<<<<<< HEAD
 //End of pseudocode
 
 //declares my variables 
-=======
-
->>>>>>> c92b758c05a396ca8324aa2019bb356354c0af30
 const includeNumbers = document.getElementById('includeNumbers');
 const includeSymbols = document.getElementById('includeSymbols');
 const includeUppercase = document.getElementById('includeUppercase');
@@ -20,30 +16,21 @@ const includeLowercase = document.getElementById('includeLowercase');
 const form = document.getElementById('passwordGeneratorForm');
 const characterAmountNumber = document.getElementById('slider');
 const submit = document.getElementById('submit');
-<<<<<<< HEAD
 const passwordLength = document.getElementById('slider')
 
 // variables for our functions
 let symbols = '!@#$%^&*()-';
-=======
-const passwordlength = document.getElementById('slider')
-
-// variables for our functions
-let symbol = '!@#$%^&*()-';
->>>>>>> c92b758c05a396ca8324aa2019bb356354c0af30
 let uppercase = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
 let lowercase = 'abcdefghijklmnopqrstuvwxyz';
 let numbers= '1234567890';
 
 
 function generatepassword () {
-<<<<<<< HEAD
 //lets the computer know all the variables we want pulled
-    let allcharacters= "ABCDEFGHIJKLMNOPQRSTUVWXYZ!@#$%^&*()-123456789abcdefghijklmnopqrstuvwxyz" ;
+let allcharacters= "ABCDEFGHIJKLMNOPQRSTUVWXYZ!@#$%^&*()-123456789abcdefghijklmnopqrstuvwxyz" ;
 //we are trying to slice characters and get a string of values that are equal to the password length
 console.log(Math.floor(Math.random()*(allcharacters.length-1)));
 document.getElementById('showpassword').innerText = allcharacters;
-/*
 console.log ("allcharacters", allcharacters);
 console.log ("uppercase",uppercase);
 console.log ("uppercase+ lowercase",uppercase + lowercase);
@@ -53,10 +40,10 @@ console.log ('lowercase',includeLowercase.checked);
 console.log ('uppercase', includeUppercase.checked)
 console.log ('numbers', includeNumbers.checked);
 console.log ('symbol', includeSymbols.checked);
-*/
 
 
    if(includeUppercase.checked && includeLowercase.checked && includeNumbers.checked && includeSymbols.checked) {
+    console.log ("a");
     let allcharacters= "ABCDEFGHIJKLMNOPQRSTUVWXYZ!@#$%^&*()-123456789abcdefghijklmnopqrstuvwxyz" ;
     document.getElementById('showpassword').innerText =Math.floor(Math.random()*(allcharacters.length-1));
 
@@ -102,65 +89,3 @@ return;
     else if (includeLowercase.checked && includeNumbers.checked) {
     }
 }
-=======
-  //lets the computer know all the variables we want pulled
-   let allcharacters= "ABCDEFGHIJKLMNOPQRSTUVWXYZ!@#$%^&*()-123456789abcdefghijklmnopqrstuvwxyz" ;
-  //we are trying to slice characters and get a string of values that are equal to the password length
-   let random = Math.floor(Math.random() * 20);
-   let newchar = allcharacters.slice(random, 20);
-   console.log (newchar);
-   console.log (allcharacters);
-   console.log (uppercase);
-   console.log (uppercase + lowercase);
-   console.log (numbers);
-   console.log (symbol);
-   
-
-   if(includeUppercase.checked && includeLowercase.checked && includeNumbers.checked && includeSymbols.checked) {
-    let random = Math.floor(Math.random() * 20);
-    //if all the above condition is met all characters will be sliced to the h3 id of showpassword
-    let allcharacters= "ABCDEFGHIJKLMNOPQRSTUVWXYZ!@#$%^&*()-123456789abcdefghijklmnopqrstuvwxyz" ;
-    // pulling the inner text of the h3 and changing it into the new sliced string
-    document.getElementById('showpassword').innerText = allcharacters.slice(random, random + +passwordLength.value);
-    console.log(allcharacters.slice(random, random + +passwordLength.value));
-}
-    else if(includeUppercase.checked && includeLowercase.checked) {
-        let random = Math.floor(Math.random() * 20);
-        let allcharacters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz" ;
-        document.getElementById('showpassword').innerText = allcharacters.slice(random, random + +passwordLength.value);
-    }
-
-    else if (includeNumbers.checked && includeSymbols.checked) {
-        let random = Math.floor(Math.random() * 20);
-        let allcharacters = numbers + symbol;
-        document.getElementById('showpassword').innerText = allcharacters.slice(random, random + +passwordLength.value);
-    }
-
-    else if (includeLowercase.checked && includeSymbols.checked) {
-        let random = Math.floor(Math.random() * 20);
-        let allcharacters = lowercase + symbol;
-        document.getElementById('showpassword').innerText = allcharacters.slice(random, random + +passwordLength.value);
-    }
-    
-     else if (includeUppercase.checked && includeSymbols.checked) {
-        let random = Math.floor(Math.random() * 20);
-        let allcharacters = uppercase + symbol;
-        document.getElementById('showpassword').innerText = allcharacters.slice(random, random + +passwordLength.value);
-    }
-
-    else if (includeUppercase.checked && includeNumbers.checked) {
-        let random = Math.floor(Math.random() * 20);
-        let allcharacters = uppercase + numbers;
-        document.getElementById('showpassword').innerText = allcharacters.slice(random, random + +passwordLength.value);
-    }
-
-    else if (includeLowercase.checked && includeNumbers.checked) {
-        let random = Math.floor(Math.random() * 20);
-        let allcharacters = lowercase + numbers;
-        document.getElementById('showpassword').innerText = allcharacters.slice(random, random + +passwordLength.value);
-    }
-}
-
-
-generatepassword();
->>>>>>> c92b758c05a396ca8324aa2019bb356354c0af30
