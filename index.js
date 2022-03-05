c7c4ed15366ee99e08a13bd3dda9695f140a72f2
Tygoog24 <97486494+Tygoog24@users.noreply.github.com>
2022-03-05 16:24:24 -0500

    first commit

--- index.js
+++ index.js
@@ -1,46 +1,92 @@
-//one way we could do it
-make a randomized generator
-//create function for it
-function randomizegenerator{
-//make a variable with all characters included
-var character="1234567890abcdefghijklmnopqrstuvxyz!#$%&'()*+,-./:;<=>?@[]^_`{|}~";
-//make a varaiable determining the password length
-var passwordlength="128";
-//make a variable thats empty so we can give value later
-var password="";
+// when a person selects one criteria for uppercase they are supposed to get an assortment of letters that are capitalized
+// when a person selects one criteria for lowercase they get an assortment of letters that are lowercased
+// when a person selects one criteria for symbols they get an assortment of symbols
+// when a person selects one criteria for numbers they get an assortment of numbers
+//when they move the range bar they can determine the amount characters for their password
+//when they click generate password we have to first check that one criteria has been selected
+//when they click generate password they are supposed to get a combination of letters uppercase/lowercase, symbols and numbers
+//when they press generate password an assortment of symbol, letters, and numbers will appear and they copy the information from the page
+//End of pseudocode
 
-//create a formula to generate random password
-for (var i = 0; i <passwordlength; i++ ){
-    var randomnumber = Math.floor(Math.random()) * character.length);
-    //substring pulls the variables from var character and will place them in different positions
-    password+= characters.substring(randomnumber, random number +1);
+//declares my variables 
+const includeNumbers = document.getElementById('includeNumbers');
+const includeSymbols = document.getElementById('includeSymbols');
+const includeUppercase = document.getElementById('includeUppercase');
+const includeLowercase = document.getElementById('includeLowercase');
+const form = document.getElementById('passwordGeneratorForm');
+const characterAmountNumber = document.getElementById('slider');
+const submit = document.getElementById('submit');
+const passwordLength = document.getElementById('slider')
+
+// variables for our functions
+let symbols = '!@#$%^&*()-';
+let uppercase = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+let lowercase = 'abcdefghijklmnopqrstuvwxyz';
+let numbers= '1234567890';
+
+
+function generatepassword () {
+//lets the computer know all the variables we want pulled
+    let allcharacters= "ABCDEFGHIJKLMNOPQRSTUVWXYZ!@#$%^&*()-123456789abcdefghijklmnopqrstuvwxyz" ;
+//we are trying to slice characters and get a string of values that are equal to the password length
+console.log(Math.floor(Math.random()*(allcharacters.length-1)));
+document.getElementById('showpassword').innerText = allcharacters;
+/*
+console.log ("allcharacters", allcharacters);
+console.log ("uppercase",uppercase);
+console.log ("uppercase+ lowercase",uppercase + lowercase);
+console.log ("numbers",numbers);
+console.log ("symbol",symbol);
+console.log ('lowercase',includeLowercase.checked);
+console.log ('uppercase', includeUppercase.checked)
+console.log ('numbers', includeNumbers.checked);
+console.log ('symbol', includeSymbols.checked);
+*/
+
+
+   if(includeUppercase.checked && includeLowercase.checked && includeNumbers.checked && includeSymbols.checked) {
+    let allcharacters= "ABCDEFGHIJKLMNOPQRSTUVWXYZ!@#$%^&*()-123456789abcdefghijklmnopqrstuvwxyz" ;
+    document.getElementById('showpassword').innerText =Math.floor(Math.random()*(allcharacters.length-1));
+
+
+
+    let uppercase = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+    console.log(Math.floor(Math.random()*(uppercase.length-1)));
+    let lowercase = 'abcdefghijklmnopqrstuvwxyz';
+    console.log(Math.floor(Math.random()*(lowercase.length-1)));
+    let symbol = '!@#$%^&*()-';
+    console.log(Math.floor(Math.random()*(symbols.length-1)));
+    let numbers= '1234567890';
+    console.log(Math.floor(Math.random()*(numbers.length-1)));
+
+
+    //if all the above condition is met all characters will be sliced to the h3 id of showpassword
+    let allcharacters= "ABCDEFGHIJKLMNOPQRSTUVWXYZ!@#$%^&*()-123456789abcdefghijklmnopqrstuvwxyz" ;
+    document.getElementById('showpassword').innerText = allcharacters;
+   }
+
+    else if(includeUppercase.checked && includeLowercase.checked) {
+        let lowercase = 'abcdefghijklmnopqrstuvwxyz';
+        let uppercase = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+        document.getElementById('showpassword').innerText = Math.floor(Math.random()*(lowercase + uppercase.length-1));
+return;
+
+    }
+    else if (includeNumbers.checked && includeSymbols.checked) {
+        let random = Math.random()*(numbers&& symbols.length-1)
+        let allcharacters = numbers + symbol;
+        document.getElementById('showpassword').innerText = Math.floor(Math.random()*(lowercase && uppercase.length-1));
+    }
+
+    else if (includeLowercase.checked && includeSymbols.checked) {
+    }
+    
+    else if (includeUppercase.checked && includeSymbols.checked) {
+    }
+
+    else if (includeUppercase.checked && includeNumbers.checked) {
+    }
+
+    else if (includeLowercase.checked && includeNumbers.checked) {
+    }
 }
-    //this is outside the for loop so each time you click create it will create a random form of numbers
-    document.getElementbyId("password").value=password;
-}
-------------//ignore the above its one way we could do it
-
-// Create variables for passwords
-//seperate the lower case from uppercase
-let lowercase="abcdefghijklmnopqrstuvxyz"
-//seperate the numbers 
-let numbers="1234567890"
-//seperate the symbols
-let symbols="!#$%&'()*+,-./:;<=>?@[]^_`{|}~"
-//seperate the uppercase
-let uppercase="ABCDEFGHIJKLMNOPQRSTUV"
-
-
-//create function to copy text
-function copytext {
-//function will go here
-}
-
-//create function to generate password
-function generatepassword{
-//function will go here
-
-//create an alert so they know they copied text
-alert("password copied")
-}
-
